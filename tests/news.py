cf626c88d638f8bdce0eb7f521cece3087bf82b5
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-08-01 10:11:38 +0300

    Initial commit. Gathering the related contrib

---
+++ tests/news.py
@@ -0,0 +1,29 @@
+from arekit.common.news.parser import NewsParser
+from arekit.common.opinions.collection import OpinionCollection
+from arekit.common.synonyms.base import SynonymsCollection
+from arekit.common.text.parser import BaseTextParser
+
+from arekit.contrib.source.rusentrel.io_utils import RuSentRelVersions
+from arekit.contrib.source.rusentrel.news_reader import RuSentRelNewsReader
+from arekit.contrib.source.rusentrel.opinions.collection import RuSentRelOpinionCollection
+
+
+def init_rusentrel_doc(doc_id, text_parser, synonyms):
+    assert(isinstance(doc_id, int))
+    assert(isinstance(text_parser, BaseTextParser))
+    assert(isinstance(synonyms, SynonymsCollection))
+
+    news = RuSentRelNewsReader.read_document(doc_id=doc_id,
+                                             synonyms=synonyms,
+                                             version=RuSentRelVersions.V11)
+
+    parsed_news = NewsParser.parse(news=news,
+                                   text_parser=text_parser)
+
+    opins_it = RuSentRelOpinionCollection.iter_opinions_from_doc(doc_id=doc_id)
+    opinions = OpinionCollection(opinions=opins_it,
+                                 synonyms=synonyms,
+                                 error_on_synonym_end_missed=True,
+                                 error_on_duplicates=True)
+
+    return news, parsed_news, opinions
---
+++ update_arekit.sh
@@ -0,0 +1,2 @@
+pip3 uninstall arekit
+pip3 install git+https://github.com/nicolay-r/AREkit@ca1e647c4acc102d1b4f57709f554ec2adbd6dc7
